ebf156749413004e31beef4f67ad718beaef5346
mattschwar03 <matt@goblackcat.com>
2024-07-22 03:49:12 -0400

    Finish the calculator app. All functions work as they should. User can only enter one decimal as well.

--- calculator.js
+++ calculator.js
@@ -18,24 +18,33 @@ const divide = function(a, b) {
   }
 }
 
-const operate = function() {
+const hasDecimal = function(value) {
+  return value % 1 == 0;
+}
+
+const operate = function(value) {
   num1 = Number(num1);
   num2 = Number(num2);
 
-  if (operator == '+') {
+  if (value == '+') {
     displayValue = add(num1, num2);
-  } else if (operator == '-') {
+  } else if (value == '-') {
     displayValue = subtract(num1, num2);
-  } else if (operator == '*') {
+  } else if (value == '*') {
     displayValue = multiply(num1, num2);
-  } else if (operator == '/') {
+  } else if (value == '/') {
     displayValue = divide(num1, num2);
   }
 
   num1 = displayValue;
   num2 = '';
-  
-  updateDisplay(displayValue);
+
+  if (!hasDecimal) {
+    updateDisplay(displayValue);
+  } else {
+    displayValue = displayValue.toFixed(5);
+    updateDisplay(displayValue);
+  }
 }
 
 const updateDisplay = function(value) {
@@ -46,27 +55,47 @@ const updateDisplay = function(value) {
 const clear = function() {
   num1 = '';
   num2 = '';
-  operator = '';
+  currOperator = '';
+  prevOperator = '';
   displayValue = '0';
   updateDisplay(displayValue);
 }
 
 let num1 = '';
 let num2 = '';
-let operator = '';
-let displayValue = '0'
+let currOperator = '';
+let prevOperator = '';
+let displayValue = '0';
+let maxNumber = 7;
+let currNumber = 0;
 
 updateDisplay(displayValue);
 
 const numbers = document.querySelectorAll('#number');
 numbers.forEach(button => {
   button.addEventListener('click', () => {
-    if (operator == '') {
-      num1 += button.value;
-      updateDisplay(num1);
+    if (currOperator == '') {
+        if (currNumber < 10) {
+          if (button.value == '.' && num1.includes('.') == false) {
+            num1 += button.value;
+            updateDisplay(num1);
+          } else if (button.value != '.') {
+            num1 += button.value;
+            updateDisplay(num1);
+          }
+          currNumber++;
+        }
     } else {
-      num2 += button.value;
-      updateDisplay(num2);
+      if (currNumber < 10) {
+        if (button.value == '.' && num2.includes('.') == false) {
+          num2 += button.value;
+          updateDisplay(num2);
+        } else if (button.value != '.') {
+          num2 += button.value;
+          updateDisplay(num2);
+        }
+        currNumber++;
+      }
     }
   });
 });
@@ -74,13 +103,26 @@ numbers.forEach(button => {
 const operators = document.querySelectorAll('#operator');
 operators.forEach(button => {
   button.addEventListener('click', () => {
-    operator = button.value;
+    if (num2 != '') {
+      prevOperator = currOperator;
+      currOperator = button.value;
+      if (prevOperator != currOperator) {
+        operate(prevOperator);
+      } else {
+        operate(currOperator);
+      }
+    } else {
+      console.log(4);
+      currOperator = button.value;
+    }
   });
 });
 
 const equals = document.querySelector('#equals');
 equals.addEventListener('click', () => {
-  operate();
+    if (num2 != '') {
+      operate(currOperator);
+    }
 });
 
 const allClear = document.querySelector('#clear');
